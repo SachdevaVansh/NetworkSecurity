6ca0cfae6f1b40b3880be97465fe8857fbbeb275
SachdevaVansh <bh2ggsipu@gmail.com>
2025-05-04 02:26:47 +0530

    Pushing the data to mongodb

--- setup.py
+++ setup.py
@@ -1,29 +1,39 @@
+'''
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project, such as its metadata, dependencies, and more
+'''
+
 from setuptools import find_packages,setup
 from typing import List
 
 def get_requirements()->List[str]:
     """
-    This function returns the list of requirements 
+    Thiss function will return list of requirements
+    
     """
-    requirement_list:List[str]=[]
+    requirement_lst:List[str]=[]
     try:
         with open('requirements.txt','r') as file:
+            #Read lines from the file
             lines=file.readlines()
+            ## Process each line
             for line in lines:
-                requirement=line.strip() # For any empty spaces 
-                if requirement and requirement!='-e .':
-                    requirement_list.append(requirement)
+                requirement=line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
     except FileNotFoundError:
-        print("requirement.txt is not found")
+        print("requirements.txt file not found")
 
-    return requirement_list
-
+    return requirement_lst
 
 setup(
     name="NetworkSecurity",
     version="0.0.1",
-    author="Vansh_Sachdeva",
-    author_email="bh2ggsipu@gmail.com",
+    author="Krish Naik",
+    author_email="krishnaik06@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements()
 )
